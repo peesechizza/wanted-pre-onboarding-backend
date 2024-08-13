6a02c013757fe5edeba0834fdd6107bda94c5532
a1dotls <meeae9408@gmail.com>
2024-08-13 16:54:31 +0900

    chore: 프로젝트 초기 구조 설정

---
+++ wanted/src/main/java/hello/wanted/WantedApplication.java
@@ -0,0 +1,13 @@
+package hello.wanted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WantedApplication.class, args);
+    }
+
+}
