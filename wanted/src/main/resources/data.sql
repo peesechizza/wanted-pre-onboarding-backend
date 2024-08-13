25b35b184886709caa0866b5b3e8030bbe20fbb6
a1dotls <meeae9408@gmail.com>
2024-08-13 18:52:54 +0900

    chore: DB 초기화 data.sql 추가

---
+++ wanted/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into company (name, nation, country) values ("네이버", "한국", "서울");
+
+insert into job_posting (company_id, position, reward, skills, description)
+	values (1, "프론트엔드 주니어 개발자", 1500000, "Python", "원티드랩에서 프론트엔드 주니어 개발자를 채용합니다. 자격요건은..");
